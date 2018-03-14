cac0e867284afcb873f14466218ba8b047c621f4
Adrian Yankov <adrian_iankov@abv.bg>
2018-03-14 19:07:36 +0100

    added remove and play delegates

--- TrafficSimulatorWpf/MainWindowViewModel.cs
+++ TrafficSimulatorWpf/MainWindowViewModel.cs
@@ -1,8 +1,32 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Windows.Input;
 
 namespace TrafficSimulatorWpf
 {
-  public class MainWindowViewModel : BindableBase
-  {
-  }
+    public class MainWindowViewModel : BindableBase
+    {
+        private DelegateCommand _playCommand;
+        public ICommand PlayCommand => _playCommand ?? (_playCommand = new DelegateCommand(Play));
+
+        private DelegateCommand _deleteCommand;
+        public ICommand DeleteCommand => _deleteCommand ?? (_deleteCommand = new DelegateCommand(Delete));
+
+        /// <summary>
+        /// Starts the simulation
+        /// </summary>
+        private void Play()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Deletes the selected Road
+        /// </summary>
+        private void Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
